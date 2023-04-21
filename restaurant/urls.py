3e3231e69ffe6359f9c3b2069a4b8f4a3adcc91e
PhanidhirajP <ppadimiti@gmail.com>
2023-04-21 11:46:45 +0530

    added api views

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, MenuView, SingleItemMenuView
 
 urlpatterns = [
-    path("", index)
+    path("", index),
+    path("menu", MenuView.as_view()),
+    path("menu/<int:pk>", SingleItemMenuView.as_view()),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from rest_framework.generics import (ListCreateAPIView,
+                                     RetrieveUpdateDestroyAPIView)
+from rest_framework.viewsets import ModelViewSet
+
+from restaurant.models import Booking, Menu
+from restaurant.serializers import BookingSerializer, MenuSerializer
 
 # Create your views here.
 
 
 def index(request):
     return render(request, "index.html", {})
+
+
+class MenuView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleItemMenuView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
